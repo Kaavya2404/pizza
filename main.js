e2329823d4ddf73df3e0da05e39ee6728758bf26
Kaavya2404 <74241997+Kaavya2404@users.noreply.github.com>
2021-04-04 21:44:22 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+menu_list_array =["chicken tandoori pizza"
+                   "Veg supreme pizza"
+                  "Panner tikka pizza"
+                   "cheese tandoori pizza"
+                   "deluxe veg pizza" ]
+
+
+ function get_menu()  {
+var htmldata;
+htmldata="<ol class- menu list>"
+
+
+
+ }                              
